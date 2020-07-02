fe2d48488bff8b66a84ee19105add543ac3f7795
Bagas Udi Sahsangka <udibagas@gmail.com>
2020-07-02 23:23:57 +0700

    commit seadanya

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.KnightOne')
+
+@section('content')
+    <x-hero />
+
+  <main id="main">
+
+    <x-about />
+
+    <x-services />
+
+    <x-cta />
+
+    <x-features />
+
+    <x-client />
+
+    <x-counter />
+
+    <x-portfolio />
+
+    <x-pricing />
+
+    <x-faq />
+
+    <x-contact />
+
+  </main><!-- End #main -->
+@endsection
